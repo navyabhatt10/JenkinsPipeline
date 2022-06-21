013330a7cb2f9e5fed1e8bc5dadec780101605f0
Navya Bhatt <navyabhatt10@gmail.com>
2022-06-21 17:51:16 +0530

    Updated

--- Project/src/App.js
+++ Project/src/App.js
@@ -3,12 +3,14 @@ import Todo from "./components/Todo";
 import Modal from "./components/Modal";
 
 function App() {
-  return <div>
+  return (<div>
     <h1>MY TODOS</h1>
     <Todo text="Learn React"/>
     <Todo text="Master React"/>
     <Todo text="Explore the full react course"/>
-    </div>;
+    <Todo text="React Completed"/>
+    </div>
+    );
 }
 
 export default App;
