49d56f4243e291179ecc02de2f52bb5598deeb0f
Navya Bhatt <navyabhatt10@gmail.com>
2022-06-21 17:38:09 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Backdrop from "./components/Backdrop";
+import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+
+function App() {
+  return <div>
+    <h1>MY TODOS</h1>
+    <Todo text="Learn React"/>
+    <Todo text="Master React"/>
+    <Todo text="Explore the full react course"/>
+    </div>;
+}
+
+export default App;
